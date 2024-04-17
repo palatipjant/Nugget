cce22db0ca9491ff2dfbb88941d528674f67f209
palatipjant <palatipjant@outlook.com>
2024-04-17 16:58:30 +0700

    Initial Commit

---
+++ Nugget/NuggetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NuggetApp.swift
+//  Nugget
+//
+//  Created by Palatip Jantawong on 17/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NuggetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
